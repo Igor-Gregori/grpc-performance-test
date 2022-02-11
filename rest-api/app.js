4c117a43119783168861fd45c7594a37dec1b741
Igor Gregori <igsilvacarlos@gmail.com>
2022-02-11 20:29:48 -0300

    fix: corrige nome de variável

--- rest-api/app.js
+++ rest-api/app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3001
 
 const githubInfo = require('../common/github-info-igor.json')
-const ramdonPersonInfo = require('../common/random-person-info.json')
+const randomPersonInfo = require('../common/random-person-info.json')
 const nasaMeteoriteData = require('../common/nasa-meteorite-data.json')
 
 app.get('/low-payload', (req, res) => {
@@ -12,7 +12,7 @@ app.get('/low-payload', (req, res) => {
 })
 
 app.get('/medium-payload', (req, res) => {
-  res.status(200).json(ramdonPersonInfo)
+  res.status(200).json(randomPersonInfo)
 })
 
 app.get('/high-payload', (req, res) => {
